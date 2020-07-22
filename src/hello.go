4e6b72c429e9b84918ec432e15387e943125baf4
ParekhAmi <amiparekh87@gmail.com>
2020-07-22 17:18:52 -0400

    Nothing changed

--- src/hello.go
+++ src/hello.go
@@ -2,29 +2,8 @@ package main
 
 import "fmt"
 
-// var a, b = 2, 2
-
-func arithmetic() {
-
-	var a, b int
-	var sum, sub, mul, mod int
-
-	fmt.Print("Enter any two numbers: ")
-	fmt.Scan(&a, &b)
-
-	sum = a + b
-	sub = a - b
-	mul = a * b
-	mod = a % b
-
-	fmt.Print("Sum : ", sum)
-	fmt.Print("\nSub : ", sub)
-	fmt.Print("\nMul : ", mul)
-	fmt.Print("\nMod : ", mod)
-}
-
 func main() {
 
-	arithmetic()
+	fmt.Println("Hello World")
 
 }
