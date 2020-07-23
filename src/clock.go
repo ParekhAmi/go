268a904298cf5dbd6e3d262e4b8122544928051b
ParekhAmi <amiparekh87@gmail.com>
2020-07-23 01:07:56 -0400

    60 minutes increase 1 hour

--- src/clock.go
+++ src/clock.go
@@ -3,21 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	//var hour = 0
+	var hour = 0
 	var min = 0
 	var sec = 0
 
-	for i := 1; i <= 60; i++ {
+	for i := 1; i <= 120; i++ {
 		sec++
+		min++
 
-		if sec > 59 {
+		if sec == 60 || sec == 120 {
 			min++
+		}
+		if min == 60 || min == 120 {
+			hour++
 		}
 
 	}
 	fmt.Println(sec)
 
-	fmt.Printf("Min is : %d", min)
+	fmt.Printf("Min is : %d\n", min)
+	fmt.Println("hour: ", hour)
 
 	/*if min > 59 {
 			hour++
@@ -27,5 +32,7 @@ func main() {
 			fmt.Printf("Time is: %d %d,%d", hour, min, sec)
 		}
 	}*/
+
+	//func (t Time) Clock() (hour, min, sec int)
 
 }
