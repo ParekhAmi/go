8bcbc0d98b4fe91e847f3d819a1af4442b931014
ParekhAmi <amiparekh87@gmail.com>
2020-07-23 20:35:26 -0400

    Simple assigning value of second

--- src/clock.go
+++ src/clock.go
@@ -1,38 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var hour = 0
+
+	var sec int
+	var min *int
+	var hour **int
+
+	min = &sec
+	hour = &min
+
+	fmt.Println("Enter number of seconds: ")
+	fmt.Scanln(&sec)
+
+	*min = sec / 60
+
+	**hour = *min / 60
+
+	fmt.Println(**hour)
+	fmt.Println(*min)
+	fmt.Println(sec)
+
+	/* var hour = 0
 	var min = 0
 	var sec = 0
 
-	for i := 1; i <= 120; i++ {
+	for i := 1; i <= 60; i++ {
 		sec++
+
+		if sec == 60 {
+			min++
+			sec = 0
+		}
+
+	}
+	for j := 1; j <= 60; j++ {
 		min++
 
-		if sec == 60 || sec == 120 {
-			min++
-		}
-		if min == 60 || min == 120 {
+		if min == 60 {
 			hour++
+			min = 0
 		}
-
 	}
 	fmt.Println(sec)
 
 	fmt.Printf("Min is : %d\n", min)
 	fmt.Println("hour: ", hour)
 
-	/*if min > 59 {
-			hour++
-		}
+	if hour > 24 {
 
-		if hour > 24 {
-			fmt.Printf("Time is: %d %d,%d", hour, min, sec)
-		}
+		fmt.Printf("Time is: %d %d,%d", hour, min, sec)
 	}*/
-
-	//func (t Time) Clock() (hour, min, sec int)
 
 }
