3a93602c57aea29236e1849f1b1de4e0deed49c4
ParekhAmi <amiparekh87@gmail.com>
2020-08-18 00:04:38 -0400

    Added more routes

--- src/simplewebapp.go
+++ src/simplewebapp.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+/*func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Golang basic web app learning..")
+}*/
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a showSnippet handler function.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a createSnippet handler function.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8000", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :8000")
+	err := http.ListenAndServe(":8000", mux)
+	log.Fatal(err)
+	/* http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8000", nil) */
+
 }
