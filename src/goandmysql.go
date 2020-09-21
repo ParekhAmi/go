7b14184054f1e3d0527ca4654c487c89a2b8407f
ParekhAmi <amiparekh87@gmail.com>
2020-09-21 01:40:50 -0400

    Trying to insert data but error in TCP connection

--- src/goandmysql.go
+++ src/goandmysql.go
@@ -7,23 +7,57 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+/*type user struct {
+	Name string
+}*/
+
 func main() {
 	fmt.Println("Go MySQL Database Connection")
 
 	// Open up our database connection.
-	// I've set up a database on my local machine using phpmyadmin.
 	// The database is called db_1
-	con, err := sql.Open("mysql", "root:example@/db_1")
+	con, err := sql.Open("mysql", "root:example@tcp(172.19.0.2:3306)/db_1")
 
-	if con != nil {
-		fmt.Println("Connection is done...")
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("Connection established..")
 	}
 
-	// if there is an error opening the connection, handle it
+	defer con.Close()
+
+	insert, err := con.Prepare("INSERT INTO Users(Id, Name) VALUES (1, 'test')")
+
+	if err != nil {
+
+		fmt.Println(err)
+		//panic(err.Error())
+	} else {
+		fmt.Println("New record inserted")
+	}
+
+	defer insert.Close()
+
+	_, err = insert.Exec(nil, "John")
+	if err != nil {
+		panic(err)
+	}
+
+	/*results, err := con.Query("SELECT UserName FROM Users")
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// defer the close till after the main function has finished executing
-	defer con.Close()
+	for results.Next() {
+		var User user
+
+		err = results.Scan(&User.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println(User.Name)
+
+	}
+	defer results.Close()*/
 }
