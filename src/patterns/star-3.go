0840c5ade57b364fd05cf1474cb0d21447d5dac5
ParekhAmi <amiparekh87@gmail.com>
2020-10-03 20:33:10 -0400

    Boat star pattern is done

--- src/patterns/star-3.go
+++ src/patterns/star-3.go
@@ -3,23 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 4; i++ {
+	/*for i := 1; i <= 4; i++ {
 		for j := 1; j <= i; j++ {
 			fmt.Print("*")
 		}
-
 		fmt.Println("")
-
 	}
-	for i := 1; i <= 4; i++ {
-		for k := 7; k >= i; k-- {
-			fmt.Print(" ")
+
+		for i := 1; i <= 4; i++ {
+			for k := 7; k >= i; k-- {
+			if k >= 5 {
+				fmt.Print(" ")
+			}
 		}
 		for l := 1; l <= i; l++ {
 			fmt.Print("*")
 		}
-		fmt.Println("")
 
+		fmt.Println("")
+		}*/
+
+	for i := 1; i <= 4; i++ {
+		for j := 1; j <= 8; j++ {
+			if j <= i || j >= 9-i {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("")
 	}
 
 }
